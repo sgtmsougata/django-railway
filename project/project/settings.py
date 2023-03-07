a890299267990b8a1a381ffc099b9bae2c26f03a
alishazin <78612608+alishazin@users.noreply.github.com>
2023-03-08 03:58:15 +0530

    ready to deploy

--- project/project/settings.py
+++ project/project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v2cwgc9i8#teg6l2r^s+a)dsl46==1re+uw-i3*$e6wcdxy96='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
